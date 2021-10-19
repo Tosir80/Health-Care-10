75299cbeabdfca5247a80ad913c7b31b02d72b15
Tosirr <tosir802637@gmail.com>
2021-10-19 18:43:45 +0600

    finally completed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,16 +3,40 @@ import { Col, Row } from 'react-bootstrap';
 
 const Footer = () => {
     return (
-        <div>
-            <Row className="bg-dark text-white py-3">
-                <Col>
-                <h4>Dental-Care</h4>
-                </Col>
-                <Col>
-                <p> <span>&copy;2021</span> Copyright By Dental-Care</p>
-                </Col>
-            </Row>
-        </div>
+      <div className="pt-5" style={{ backgroundColor: '#0B2C4B',color:"white" }}>
+        <Row xs={1} md={3}>
+          <Col>
+            <h4>Dental-Care</h4>
+            <h6 className="py-1">Location:Dhaka,Bangladesh</h6>
+            <h6>Phone : +8801779794012</h6>
+            <h6>Email:tosiruzzamantosir@gmail.com</h6>
+          </Col>
+          <Col>
+            <h3 className='text-info'>Support </h3>
+            <p>Our Dentist</p>
+            <p>Terms of Services</p>
+            <p>Privacy Policy</p>
+            <p>Our Pricing</p>
+          </Col>
+          <Col>
+            <h3 className='text-info'> Our Services </h3>
+            <p>Cosmetic Dentistry</p>
+            <p>General Dentistry</p>
+            <p>Certified Dentist</p>
+            <p>New Technology</p>
+          </Col>
+        </Row>
+        <Row className='bg-dark text-white py-3'>
+          <Col>
+            <h4>Dental-Care</h4>
+          </Col>
+          <Col>
+            <p>
+              <span>&copy;2021</span> Copyright By Dental-Care
+            </p>
+          </Col>
+        </Row>
+      </div>
     );
 };
 
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,9 +4,8 @@ import { useHistory, useLocation } from 'react-router';
 import { Col, Row } from 'react-bootstrap';
 import { AiFillGooglePlusCircle } from 'react-icons/ai';
 import {  BiUserCircle } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
 import LoginWIthEmail from '../LoginwithEmail/LoginWIthEmail';
-
+import './Login.css'
 
 const Login = () => {
     const {user,signInUsingGoogle}=useAuth()
@@ -21,45 +20,43 @@ signInUsingGoogle().then((result)=>{history.push(redirect)})
   }
 
     return (
-      <div>
-        <Row  className='py-4 justify-content-around align-items-center'>
-          <Col>
-            <LoginWIthEmail></LoginWIthEmail>
-          </Col>
-
-
-
-
-
-          <Col>
-            {user?.email && (
-              <div>
-                <h3>
-                  <span className='fs-1 text-primary'>
-                    <BiUserCircle />{' '}
-                  </span>{' '}
-                  Your Profile
-                </h3>
-                <div className='card p-2 pt-0 m-5 bg-info text-center'>
-                  <div>
-                    {' '}
-                    <img
-                      className='rounded-circle w-50 p-2'
-                      src={user?.photoURL}
-                      alt=''
-                    />
+      <div className="container">
+        <div className='login'>
+          <h2 className='py-3'>Welcome to Login</h2>
+          <Row className='py-4 justify-content-around align-items-center'>
+            <Col>
+              <LoginWIthEmail></LoginWIthEmail>
+            </Col>
+            <Col>
+              {user?.email && (
+                <div>
+                  <h3>
+                    <span className='fs-1 text-primary'>
+                      <BiUserCircle />{' '}
+                    </span>{' '}
+                    Your Profile
+                  </h3>
+                  <div className='card p-2 pt-0 m-5 bg-info text-center'>
+                    <div>
+                      {' '}
+                      <img
+                        className='rounded-circle w-50 p-2'
+                        src={user?.photoURL}
+                        alt=''
+                      />
+                    </div>
+                    <h3> Name : {user?.displayName}</h3>
+                    <p>Gmail: {user?.email}</p>
                   </div>
-                  <h3> Name : {user?.displayName}</h3>
-                  <p>Gmail: {user?.email}</p>
                 </div>
-              </div>
-            )}
-            <button className='btn btn-primary' onClick={handleGoogleBtn}>
-              <AiFillGooglePlusCircle className='text-warning fs-1 ' /> Login
-              with Google
-            </button>
-          </Col>
-        </Row>
+              )}
+              <button className='btn btn-primary' onClick={handleGoogleBtn}>
+                <AiFillGooglePlusCircle className='text-warning fs-1 ' /> Login
+                with Google
+              </button>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
 };
